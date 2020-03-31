5c1bb147be77850592d0b3e456160748ac8f9bc4
Zealer Chen <xinxin8816@gmail.com>
2020-03-31 18:27:18 +0800

    Fix can't pull bug

--- creater.ps1
+++ creater.ps1
@@ -1,6 +1,6 @@
-$name="xinrclone"
+$name="xin2rclone"
 
-docker pull xinxin8816/xinrclone
+docker pull xinxin8816/xinrclone:0.4
 heroku create -a $name
 heroku container:push web -a $name
 heroku container:release web -a $name
